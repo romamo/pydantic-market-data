a07c7c133402fb943a35ef1596538780186c591e
Romamo <romamo@yandex.ru>
2026-01-30 22:07:22 +0100

    fix: expose __version__

--- pydantic_market_data/__init__.py
+++ pydantic_market_data/__init__.py
@@ -1,3 +1,10 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("pydantic-market-data")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
 from .interfaces import DataSource
 from .models import (
     OHLCV,
