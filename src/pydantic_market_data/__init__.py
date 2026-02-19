bdcd14ea0ac7f5ab933a3f80c4b6492c48143765
Romamo <romamo@yandex.ru>
2026-02-19 10:15:04 +0200

    chore: release v0.1.10

--- src/pydantic_market_data/__init__.py
+++ src/pydantic_market_data/__init__.py
@@ -3,7 +3,7 @@ import importlib.metadata
 try:
     __version__ = importlib.metadata.version("pydantic-market-data")
 except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.1.7"
+    __version__ = "0.1.10"
 
 from .interfaces import DataSource
 from .models import (
